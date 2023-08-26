b5d0406059d51109f7d3beb19ea695628abbf58f
Christoph Jean-Luc Schneider <kopilot.schneider@protonmail.com>
2023-08-26 22:20:36 +0200

    setup website

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<slot />
+
+<style>
+	:root,
+	:global(body) {
+		font-family: sans-serif;
+		color-scheme: light dark;
+		margin: 0;
+		box-sizing: border-box;
+	}
+
+	:global(a) {
+		color: unset;
+		text-decoration: none;
+	}
+
+	:global(a:hover) {
+		color: rgb(0, 81, 255);
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,71 @@
+<main>
+	<img src="/coffee-license.svg" alt="coffee license logo" />
+	<h1>Coffee License</h1>
+	<code>
+		Coffee License 1.0
+		<br />
+		<br />
+
+		Use this software in any way you like. If we meet, buy us a coffee.
+		<br />
+		<br />
+
+		We disclaim all warranties with regard to this software. In no event are we liable for any
+		damages whatsoever.
+	</code>
+	<br />
+	<br />
+
+	<h2>🫴 Permissions</h2>
+	<p>Use this software in any way you like.</p>
+
+	<h2>🤝 Conditions</h2>
+	<p>If we meet, buy us a coffee.</p>
+
+	<h2>☝️ Limitations</h2>
+	<p>
+		We disclaim all warranties with regard to this software. In no event are we liable for any
+		damages whatsoever.
+	</p>
+	<br />
+	<br />
+
+	<div>
+		<a href="https://github.com/coffee-license/website" target="_blank" rel="noopener noreferrer">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor">
+				<g
+					stroke-width="1.5"
+					stroke="currentColor"
+					fill="none"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<path
+						d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"
+					/>
+				</g>
+			</svg>
+		</a>
+	</div>
+</main>
+
+<style>
+	main {
+		width: 100%;
+		max-width: 32rem;
+		margin: 0 auto;
+		padding: 2rem;
+		box-sizing: border-box;
+		font-size: 1.25rem;
+	}
+
+	img {
+		width: 8rem;
+	}
+
+    svg {
+        width: 2em;
+        height: 2em;
+        stroke: currentColor;
+    }
+</style>
